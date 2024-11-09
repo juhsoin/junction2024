040fe81a818199888512c734361d9571a98d3ad1
oskarikera01 <oskarikera@gmail.com>
2024-11-09 12:54:40 +0200

    Use ticket grid in home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
-// import TheWelcome from '../components/TheWelcome.vue'
+import TicketGrid from '@/components/TicketGrid.vue';
+import { VContainer } from 'vuetify/components';
+
+// plaeholder for demo purposes
+const ticketInfo = [
+	{
+		id: '1',
+		title: 'First ticket',
+		description: 'First Description'
+	},
+	{
+		id: '2',
+		title: 'First ticket',
+		description: 'First Description'
+	},
+	{
+		id: '3',
+		title: 'First ticket',
+		description: 'First Description'
+	},
+	{
+		id: '4',
+		title: 'First ticket',
+		description: 'First Description'
+	},
+]
 </script>
 
 <template>
-	<main>
-		<div>Kontsaa</div>
-	</main>
+	
+<VContainer>
+	<h1>Your Tickets</h1>
+	<TicketGrid :tickets="ticketInfo">
+	</TicketGrid>
+</VContainer>
 </template>
