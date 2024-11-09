36307cdd102aa417be9629bd8986d7a5c66473cd
oskarikera01 <oskarikera@gmail.com>
2024-11-09 16:43:57 +0200

    Remove redundant components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 import Footer from './components/Footer.vue'
 import Header from './components/Header.vue';
 import { VApp, VMain } from 'vuetify/components';
