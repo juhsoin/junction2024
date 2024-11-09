5991d119667607e94a0eae2131ab08fc713cfd3d
oskarikera01 <oskarikera@gmail.com>
2024-11-09 10:59:34 +0200

    Add Header and Footer components

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { VFooter, VContainer, VRow, VCol, VDivider } from 'vuetify/components';
+
+const currentYear = new Date().getFullYear();
+</script>
+
+<template>
+  <VFooter app color="primary" class="footer">
+      <VContainer class="text-center">
+        <span>&copy; {{ new Date().getFullYear() }} Fingrid Service Hub</span>
+      </VContainer>
+    </VFooter>
+</template>
+
+<style scoped>
+/* Add any custom styling here */
+.v-footer {
+}
+</style>
