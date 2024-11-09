2abb8d0527db294b70f5157f23b13c83f61006ba
oskarikera01 <oskarikera@gmail.com>
2024-11-09 12:54:20 +0200

    Add ticket grid component to iteratively render ticket cards based on ticket lists

---
+++ frontend/src/components/TicketGrid.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { VCol, VRow, VContainer } from 'vuetify/components';
+import TicketCard from './TicketCard.vue';
+
+interface ticketCardInfo {
+    id: string;
+    title: string;
+    description: string;
+}
+
+const props = defineProps<{
+  tickets: ticketCardInfo[];
+}>();
+
+const emit = defineEmits<{
+  (e: 'ticket-action', ticket: { id: number; title: string; description: string }): void;
+}>();
+
+const tickets = props.tickets;
+
+</script>
+
+<template>
+   <VContainer>
+    <VRow :gutter="0" class="ticket-row">
+      <VCol
+        v-for="ticket in tickets"
+        :key="ticket.id"
+        cols="4"
+        >
+        <TicketCard
+          :title="ticket.title"
+          :description="ticket.description"
+        />
+      </VCol>
+    </VRow>
+   </VContainer>
+</template>
+
+<style scoped>
+.ticket-row {
+    width: 1000px;
+}
+</style>
