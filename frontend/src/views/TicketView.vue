9d203996bb9dde7b98c947f10f09ddebc903f8b8
oskarikera01 <oskarikera@gmail.com>
2024-11-09 17:34:50 +0200

    Add tickets view to router

---
+++ frontend/src/views/TicketView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import KanbanTable from '@/components/KanbanTable.vue';
+import { VContainer } from 'vuetify/components';
+
+// plaeholder for demo purposes
+const ticketInfo = [
+	{
+		id: '1',
+		title: 'First ticket',
+		description: 'First Description',
+        scope: "In progress",
+	},
+	{
+		id: '2',
+		title: 'First ticket',
+		description: 'First Description',
+        scope: "In progress",
+	},
+	{
+		id: '3',
+		title: 'First ticket',
+		description: 'First Description',
+        scope: "In progress",
+	},
+	{
+		id: '4',
+		title: 'First ticket',
+		description: 'First Description',
+        scope: "In progress",
+	},
+]
+</script>
+
+<template>
+    <VContainer class="kanban-table">
+        <h1>Tickets workflow view</h1>
+        <KanbanTable :tickets="ticketInfo"></KanbanTable>
+    </VContainer>
+</template>
+
+<style scoped>
+</style>
