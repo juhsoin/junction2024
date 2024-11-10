5c750333fd66bf7bcdb4bec0c41a2cd8c5369f0d
nuhakala <koiskis@mailbox.org>
2024-11-10 02:11:21 +0200

    add proper category filters

--- frontend/src/views/TicketView.vue
+++ frontend/src/views/TicketView.vue
@@ -4,14 +4,35 @@ import AddNewTicket from '@/components/AddNewTicket.vue';
 import KanbanTable from '@/components/KanbanTable.vue';
 import { useFilterStore } from '@/stores/filterStore';
 import { onMounted, ref } from 'vue';
-import { VSelect, VContainer } from 'vuetify/components';
+import { VSelect, VContainer, VCheckbox } from 'vuetify/components';
 
 const store = useFilterStore();
 
 const filter = ref<string | null>(null);
 const tickets = ref<ITicket[]>([]);
 const loading = ref(true);
-const items = ['category 1', 'category 2', 'my tickets']
+const items = [
+"Käyttöpaikkaprosessi"       ,
+"Tuoteprosessi"              ,
+"Mittaustietoprosessi"       ,
+"Asiakastietoprosessi"       ,
+"Sopimusprosessi"            ,
+"Sanomahallinta"             ,
+"Yleinen hallinta"           ,
+"Tietosuoja"                 ,
+"Kytkentä/katkaisuprosessi"  ,
+"Laskutustiedot"             ,
+"Valtuutusprosessi"          ,
+"Tietoturva"                 ,
+"Roolitus"                   ,
+"Mittaustiedonhallinta"      ,
+"Raportointi"                ,
+"Toimeksiantoprosessi"       ,
+"Loppuasiakaskäyttöliittymä" ,
+"Laskennat"                  ,
+"Osapuolitiedot"             ,
+"Kuormanohjausprosessi"      
+]
 const key = ref('test')
 
 onMounted(() => {
@@ -24,6 +45,12 @@ onMounted(() => {
 const toggleFilter = async (value: string) => {
 	store.$state.filter.categories = value;
 	tickets.value = await fetchTickets();
+}
+
+const toggle = (value: bool) => {
+	if (value) {
+		store.$state.filter. = value;
+	}
 }
 </script>
 
@@ -38,6 +65,7 @@ const toggleFilter = async (value: string) => {
 			closable-chips
 		></VSelect>
 		<AddNewTicket class="add-ticket-dialog"/>
+		<VCheckbox label="Show only my tickets" @change="toggle(value)"></VCheckbox>
 		<div v-if="loading">Loading...</div>
         <KanbanTable v-else :tickets="tickets"></KanbanTable>
     </VContainer>
